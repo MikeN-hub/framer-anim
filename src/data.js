1193218e138124987cbfaebbf76b4c513f5ea962
MikeN <mikes.navr@gmail.com>
2022-02-24 08:40:09 +0200

    just merge chandges in data.js and app.js a little

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
 export const data = [
   { category: 'cars', title: 'BMW' },
-<<<<<<< HEAD
   { category: 'cars', title: 'Volvo' },
   { category: 'cars', title: 'Mustang' },
   { category: 'fruits', title: 'Melon' },
@@ -9,14 +8,4 @@ export const data = [
   { category: 'animals', title: 'Humster' },
   { category: 'fruits', title: 'Peach' },
   { category: 'fruits', title: 'Pineapple' },
-=======
-  { category: 'cars', title: 'Mercedes' },
-  { category: 'cars', title: 'Kia' },
-  { category: 'fruits', title: 'Banana' },
-  { category: 'fruits', title: 'Peach' },
-  { category: 'fruits', title: 'Waterlemon' },
-  { category: 'animals', title: 'Cat' },
-  { category: 'animals', title: 'Dog' },
-  { category: 'animals', title: 'Humster' },
->>>>>>> e0d1e245e2c6d15369bdcfe0a462a23066248325
 ]
